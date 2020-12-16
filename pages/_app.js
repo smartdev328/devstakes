2676faafdaee8ed60d1a911641285e2ee098f4e0
smartdev328 <hitgoal312@gmail.com>
2020-12-16 12:03:50 +0400

    Add tslint scripts command and sitemap

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import 'antd/dist/antd.css'
-import '@styles/globals.css'
-import { wrapper } from '@redux/store'
+import '../styles/globals.css'
+import { wrapper } from '../redux/store'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
