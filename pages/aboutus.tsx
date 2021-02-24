afc8c8c3954f2bb529cd9ee449a47400eaa9be00
Abdul Rehman <abdul.rehman@zweidevs.com>
2021-02-24 19:55:16 +0500

    About US: edit about us page

--- pages/aboutus.tsx
+++ pages/aboutus.tsx
@@ -13,80 +13,79 @@ export default function AboutUs({ token, subscriptions }: PageProps) {
       <AppLayout token={token} subscriptions={subscriptions} bgColor={'#ffffff'}>
         <div className={styles.container}>
           <div className={styles.content}>
-            <h4>About Us</h4>
-            <p>
+            <h4 className={styles.aboutUs_headings}>About Us</h4>
+            <p className={styles.ourPurpose_details}>
               The Daily Stakes, Inc. is a North American digital sports consulting & media company
               focused on the rapidly evolving sports betting and daily fantasy sports landscape. The
               company uses data driven models to provide users with predictive sports analytics,
               credible bet recommendations, premium content, and advanced bankroll management
-              strategies via its membership packages.{' '}
+              strategies via its membership packages.
             </p>
-            <h4>Our Story</h4>
-            <p>
+            <h4 className={styles.aboutUs_headings}>Our Story</h4>
+            <p className={styles.ourPurpose_details}>
               Formally established in 2021 upon forming a partnership amongst professionals working
               in the finance & consulting space, TheDailyStakes is rapidly becoming one of the most
               well- known names in the industry.
             </p>
 
-            <p>
+            <p className={styles.ourPurpose_details}>
               Our deep-rooted passion for sports & quantitative analyses were the driving pillars
               leading to the creation of TheDailyStakes. Being experienced sports bettors ourselves,
               we observed time and time again a major gap in the industry: *Providing credible bet
               recommendations backed by hard data while simultaneously educating the bettor.
             </p>
 
-            <p>
+            <p className={styles.ourPurpose_details}>
               Our team continues to grow as the ever-changing sports betting and DFS landscape
               evolves with the legalization of sports betting emerging in North America.
             </p>
-            <h4>Our Mission</h4>
-            <p>
+            <h4 className={styles.aboutUs_headings}>Our Mission</h4>
+            <p className={styles.ourPurpose_details}>
               To be the sports consulting industry leader by providing our users with the industry’s
               most versatile content and insightful recommendations.
             </p>
-            <h4>Our Purpose HOLD</h4>
+            <h4 className={styles.aboutUs_headings}>Our Purpose</h4>
             <p>
-              <ul>
-                <li>Educating the Bettor</li>
-                <br></br>
-                <p>
-                  The old saying holds true: Knowledge is Power. We don’t simply recommend picks and
-                  expect users to bet mindlessly. We include detailed stats & trends behind the
-                  selection of each pick.{' '}
-                </p>
-                <p>
-                  Furthermore, we aim to continuously provide users with content in order to arm
-                  them to become more insightful bettors. This is done through our weekly pro tip,
-                  ongoing sports betting articles, & bankroll management tactics that users can
-                  constantly add to their repertoire.
-                </p>
-                <li>Providing Credibility</li>
-                <br></br>
-                <p>
-                  Although the Supreme Court legalized sports betting in America on a state level,
-                  the sports consulting industry remains largely unregulated. Self proclaimed
-                  “experts” have emerged selling picks using misleading marketing tactics and no
-                  transparency.
-                </p>
-                <p>
-                  We self-regulate ourselves in an unrelated part of the industry by being 100%
-                  transparent through our automated record tracker. A majority of “experts” only
-                  provide a pick. Our value proposition is more than just picks. No shady marketing
-                  tactics are used here at TheDailyStakes. Our goal is to provide our users with the
-                  most credible and honest user experience available.
-                </p>
-                <li>Providing Value</li>
-                <br></br>
-                <p>
-                  Our ultimate goal is to provide value to our users in as my facets as possible. We
-                  aim to continuously absorb feedback and enhance our value offering to what our
-                  users want. We embrace the importance of innovation and adaptability here at
-                  TheDailyStakes and this remains at the forefront of our priorities.
-                </p>
-              </ul>
+              <h5 className={styles.ourPurpose_subheadings}>EDUCATING THE BETTOR</h5>
+
+              <p className={styles.ourPurpose_details}>
+                The old saying holds true: Knowledge is Power. We don’t simply recommend picks and
+                expect users to bet mindlessly. We include detailed stats & trends behind the
+                selection of each pick.
+              </p>
+              <p className={styles.ourPurpose_details}>
+                Furthermore, we aim to continuously provide users with content in order to arm them
+                to become more insightful bettors. This is done through our weekly pro tip, ongoing
+                sports betting articles, & bankroll management tactics that users can constantly add
+                to their repertoire.
+              </p>
+
+              <h5 className={styles.ourPurpose_subheadings}>PROVIDING CREDIBILITY</h5>
+
+              <p className={styles.ourPurpose_details}>
+                Although the Supreme Court legalized sports betting in America on a state level, the
+                sports consulting industry remains largely unregulated. Self proclaimed “experts”
+                have emerged selling picks using misleading marketing tactics and no transparency.
+              </p>
+              <p className={styles.ourPurpose_details}>
+                We self-regulate ourselves in an unrelated part of the industry by being 100%
+                transparent through our automated record tracker. A majority of “experts” only
+                provide a pick. Our value proposition is more than just picks. No shady marketing
+                tactics are used here at TheDailyStakes. Our goal is to provide our users with the
+                most credible and honest user experience available.
+              </p>
+
+              <h5 className={styles.ourPurpose_subheadings}>PROVIDING VALUE</h5>
+
+              <p className={styles.ourPurpose_details}>
+                Our ultimate goal is to provide value to our users in as my facets as possible. We
+                aim to continuously absorb feedback and enhance our value offering to what our users
+                want. We embrace the importance of innovation and adaptability here at
+                TheDailyStakes and this remains at the forefront of our priorities.
+              </p>
             </p>
 
-            <p>
+            <p className={styles.ourPurpose_details}>
               For more information, please visit our <a href="/faqs">FAQ Page</a> or{' '}
               <a href="/contact-us">Contact Us</a> directly!
             </p>
