383bdae6acd5f8d5eedad7fffdbb1938d4a177b6
smartdev328 <hitgoal312@gmail.com>
2021-03-03 21:12:23 +0400

    Implement new cart login flow

--- components/CartSignupForm.tsx
+++ components/CartSignupForm.tsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'antd';
 
-import { CreateUserType, CreateUserValidateType } from '@type/Users';
+import { CreateUserType, CreateUserValidateType, UserProfile } from '@type/Users';
 import { validateEmail } from '@utils/common';
 
 import styles from './LoginModal.module.css';
 
 type CartRegistration = {
   onCartLogin: () => void;
+  showLoginForm: boolean;
+  userProfile: UserProfile | undefined;
   signupFormChanged: (_: CreateUserType, _valid: boolean) => void;
 };
 
-export default function CartRegistration({ onCartLogin, signupFormChanged }: CartRegistration) {
+export default function CartRegistration({ showLoginForm, userProfile, onCartLogin, signupFormChanged }: CartRegistration) {
   const [formData, setFormData] = useState<CreateUserType>({
     username: undefined,
     first_name: undefined,
@@ -31,6 +33,25 @@ export default function CartRegistration({ onCartLogin, signupFormChanged }: Car
     password: true,
     verify_password: true
   });
+
+  useEffect(() => {
+    if (userProfile && showLoginForm) {
+      const names = userProfile.full_name?.split(' ') || ['', ''];
+      const data = {
+        username: userProfile.username,
+        first_name: names[0],
+        last_name: names[1],
+        full_name: userProfile.full_name,
+        email: userProfile.email,
+        mobile_number: userProfile.mobile_number,
+        provider: userProfile.provider,
+        password: 'undefined',
+        verify_password: 'undefined'
+      };
+      setFormData(data);
+      validateForm(data);
+    }
+  }, [showLoginForm, userProfile]);
 
   const changeFormData = (name: keyof CreateUserType, e: React.FormEvent<HTMLInputElement>) => {
     const { value } = e.currentTarget;
@@ -98,23 +119,32 @@ export default function CartRegistration({ onCartLogin, signupFormChanged }: Car
     } else {
       newValidation.verify_password = true;
     }
-    console.log('--- newValidation', isValid);
     setFormValidation(newValidation);
     signupFormChanged(formData, isValid);
   };
 
   return (
     <div>
-      <h2>Registration</h2>
-      <Row align={'middle'} className={styles.register_row}>
-        <span>Returning Customer?&nbsp;&nbsp;</span>
-        <a onClick={onCartLogin}>Click here to login</a>
-      </Row>
+      {!showLoginForm && (
+        <>
+          <h2 className={styles.cartSignupTitle}>Create an Account</h2>
+          <p className={styles.cartSignupDesc}>In order to access your Member Dashboard where you can access your membership picks</p>
+          <Row align={'middle'} className={styles.register_row}>
+            <span>Returning Customer?&nbsp;&nbsp;</span>
+            <a onClick={onCartLogin}>Click here to login</a>
+          </Row>
+        </>
+      )}
+      {showLoginForm && (
+        <h2 className={styles.cartSignupTitle}>Billing Info</h2>
+      )}
       <Row>
         <Col span={24} className={styles.formGroup}>
           <label>User Name*</label>
           <input
             name="username"
+            value={formData.username}
+            disabled={showLoginForm}
             className={formValidation.username ? '' : styles.error}
             placeholder="ie: CashmeOut91"
             onChange={(e) => changeFormData('username', e)}
@@ -126,6 +156,8 @@ export default function CartRegistration({ onCartLogin, signupFormChanged }: Car
           <label>First Name*</label>
           <input
             name="first_name"
+            value={formData.first_name}
+            disabled={showLoginForm}
             className={formValidation.first_name ? '' : styles.error}
             placeholder="ie: Johnny"
             onChange={(e) => changeFormData('first_name', e)}
@@ -135,6 +167,8 @@ export default function CartRegistration({ onCartLogin, signupFormChanged }: Car
           <label>Last Name*</label>
           <input
             name="last_name"
+            value={formData.last_name}
+            disabled={showLoginForm}
             className={formValidation.last_name ? '' : styles.error}
             placeholder="ie: Cash"
             onChange={(e) => changeFormData('last_name', e)}
@@ -146,6 +180,8 @@ export default function CartRegistration({ onCartLogin, signupFormChanged }: Car
           <label>Email*</label>
           <input
             name="email"
+            value={formData.email}
+            disabled={showLoginForm}
             className={formValidation.email ? '' : styles.error}
             type="email"
             placeholder="ie: j.cash@gmail.com"
@@ -158,33 +194,37 @@ export default function CartRegistration({ onCartLogin, signupFormChanged }: Car
           <label>Mobile Number</label>
           <input
             name="mobile_number"
+            value={formData.mobile_number}
+            disabled={showLoginForm}
             placeholder="1-111-111-1111"
             onChange={(e) => changeFormData('mobile_number', e)}
           />
         </Col>
       </Row>
-      <Row className={styles.rowWithTwoChild} justify="space-between">
-        <Col span={12} className={styles.formGroup}>
-          <label>Password*</label>
-          <input
-            name="password"
-            className={formValidation.password ? '' : styles.error}
-            type="password"
-            placeholder="ie: **********"
-            onChange={(e) => changeFormData('password', e)}
-          />
-        </Col>
-        <Col span={12} className={styles.formGroup}>
-          <label>Verify Password*</label>
-          <input
-            name="verify_password"
-            className={formValidation.verify_password ? '' : styles.error}
-            type="password"
-            placeholder="ie: **********"
-            onChange={(e) => changeFormData('verify_password', e)}
-          />
-        </Col>
-      </Row>
+      {!showLoginForm && (
+        <Row className={styles.rowWithTwoChild} justify="space-between">
+          <Col span={12} className={styles.formGroup}>
+            <label>Password*</label>
+            <input
+              name="password"
+              className={formValidation.password ? '' : styles.error}
+              type="password"
+              placeholder="ie: **********"
+              onChange={(e) => changeFormData('password', e)}
+            />
+          </Col>
+          <Col span={12} className={styles.formGroup}>
+            <label>Verify Password*</label>
+            <input
+              name="verify_password"
+              className={formValidation.verify_password ? '' : styles.error}
+              type="password"
+              placeholder="ie: **********"
+              onChange={(e) => changeFormData('verify_password', e)}
+            />
+          </Col>
+        </Row>
+      )}
     </div>
   );
 }
--- pages/cart.tsx
+++ pages/cart.tsx
@@ -21,7 +21,7 @@ import { BillingPlan, Package } from '@type/Packages';
 import { ReduxState } from '@redux/reducers';
 import { MinusIcon } from '@components/SvgIcons';
 import CheckoutAPIs from '@apis/checkout.apis';
-import { CreateUserType, LoginUserType } from '@type/Users';
+import { CreateUserType, LoginUserType, UserProfile } from '@type/Users';
 import { PACKAGE_NAMES } from '@constants/';
 
 function HeroBanner() {
@@ -201,15 +201,11 @@ function PlanDropdown({
 
 export default function Cart({ packages, token, subscriptions }: PageProps) {
   const { items: cartItems } = useSelector((state: ReduxState) => state.cart);
-  const { error, loading } = useSelector((state: ReduxState) => state.user);
+  const { error, loading, profile } = useSelector((state: ReduxState) => state.user);
   const [tempCartItems, setTempCartItems] = useState<CartItem[]>([]);
   const [proceeding, setProceeding] = useState<boolean>(false);
   const [formView, setFormView] = useState<string>('');
   const [isSignupFormValid, setSignUpFormValid] = useState<boolean>(false);
-  const [loginFormData, setLoginFormData] = useState<LoginUserType>({
-    email: undefined,
-    password: undefined
-  });
   const [signupFormData, setSignupFormData] = useState<CreateUserType>({
     username: undefined,
     first_name: undefined,
@@ -222,7 +218,9 @@ export default function Cart({ packages, token, subscriptions }: PageProps) {
     verify_password: undefined
   });
   const [formSubmitted, setFormSubmitted] = useState<boolean>(false);
+  const [isTryingLogin, setIsTryingLogin] = useState<boolean>(false);
   const [cartForVisitor] = useState<boolean>(!token);
+  const [userProfile, setUserProfile] = useState<UserProfile | undefined>();
 
   const dispatch = useDispatch();
   const stripe = useStripe();
@@ -234,6 +232,9 @@ export default function Cart({ packages, token, subscriptions }: PageProps) {
   useEffect(() => {
     if (error === null && !loading && formSubmitted) {
       proceedCheckout();
+    }
+    if (error === null && !loading && isTryingLogin && profile) {
+      setUserProfile(profile);
     }
     if (error && !loading && formView === 'LOGIN') {
       notification['error']({
@@ -320,14 +321,7 @@ export default function Cart({ packages, token, subscriptions }: PageProps) {
     //
     setProceeding(true);
     if (formView === 'LOGIN') {
-      dispatch({
-        type: 'LOGIN_USER',
-        payload: {
-          identifier: loginFormData.email,
-          password: loginFormData.password
-        }
-      });
-      setFormSubmitted(true);
+      proceedCheckout();
     } else {
       dispatch({
         type: 'SIGNUP_USER',
@@ -340,15 +334,19 @@ export default function Cart({ packages, token, subscriptions }: PageProps) {
     }
   };
 
-  const cartLoginFormChanged = (formData: LoginUserType, isValid: boolean) => {
-    setLoginFormData(formData);
-    setSignUpFormValid(isValid);
-  };
-
   const cartSignupFormChanged = (formData: CreateUserType, isValid: boolean) => {
     setSignupFormData(formData);
     setSignUpFormValid(isValid);
   };
+  const onCartLogin = () => {
+    if (formView === 'LOGIN') {
+      setFormView('');
+    } else {
+      setFormView('LOGIN');
+      setIsTryingLogin(true);
+      dispatch({ type: 'OPEN_LOGIN_MODAL' });
+    }
+  }
 
   return (
     <>
@@ -367,22 +365,12 @@ export default function Cart({ packages, token, subscriptions }: PageProps) {
             <div className={styles.cartItems}>
               {cartForVisitor && (
                 <>
-                  {formView === '' && (
-                    <>
-                      <CartSignupForm
-                        signupFormChanged={cartSignupFormChanged}
-                        onCartLogin={() => setFormView('LOGIN')}
-                      />
-                    </>
-                  )}
-                  {formView === 'LOGIN' && (
-                    <>
-                      <CartLoginForm
-                        loginFormChanged={cartLoginFormChanged}
-                        onCartSignup={() => setFormView('')}
-                      />
-                    </>
-                  )}
+                  <CartSignupForm
+                    showLoginForm={formView === 'LOGIN'}
+                    userProfile={userProfile}
+                    signupFormChanged={cartSignupFormChanged}
+                    onCartLogin={onCartLogin}
+                  />
                   <br />
                   <br />
                 </>
