d1fc227926b00efbbde77bcbf2e05de8de2b5939
Abdul Rehman <abdul.rehman@zweidevs.com>
2021-03-05 16:08:22 +0500

    add left card

--- components/LockSportsCard.tsx
+++ components/LockSportsCard.tsx
@@ -8,9 +8,24 @@ const LockSportsCard = () => {
       <div className={styles.subscribenow_desc}>
         You need to purchase this package in order to view the picks
       </div>
-      <Row>
+      <Row className={styles.rowClass}>
         <Col lg={10} className={styles.leftCard}>
-          <div></div>
+          <div className={styles.leftOuterDiv}>
+            <p className={styles.vipText}>VIP ALL ACCESS CARD</p>
+            <p className={styles.allPlayText}>ALL PLAYS. ALL SPORTS</p>
+
+            <div className={styles.imageAndDetailDiv}>
+              <img src="/images/fanduel.png" alt="Fanduel Logo" width={107} height={26} />
+              <div>
+                <p className={styles.bestValueText}> best Value!</p>
+                <p className={styles.dailyCardText}>
+                  *The Daily Card inclues TheDailyStakes Guaranteed Protection.{' '}
+                </p>
+              </div>
+            </div>
+
+            <Button className={styles.subscribe_btn}>Go to Shop Page</Button>
+          </div>
         </Col>
         <Col lg={4}>
           <p className={styles.orText}>Or</p>
