6163694bdade7774fdd38d73bee48835443c5553
kapies <kapies.vallipuram@gmail.com>
2021-03-15 20:26:54 -0400

    fix schedule

--- components/BannerSportsAndMatches.tsx
+++ components/BannerSportsAndMatches.tsx
@@ -1,137 +1,134 @@
 /* eslint-disable react/display-name */
-import React, { createRef, RefObject, useState } from 'react';
-import { Menu } from 'antd';
-import { CarouselRef } from 'antd/lib/carousel';
-import { F1_SVG, NBA_SVG, NFL_SVG, UFC_SVG, SOCCER_SVG, MLB_SVG } from '@components/SportIcons';
-import { SportInfoType } from '@type/Main';
+import React from 'react';
+//import { F1_SVG, NBA_SVG, NFL_SVG, UFC_SVG, SOCCER_SVG, MLB_SVG } from '@components/SportIcons';
 import styles from './BannerSportsAndMatches.module.css';
 
-const SPORTS_INFO = [
-  {
-    name: 'NBA',
-    id: 'NBA',
-    background: '#EC4C15',
-    logo: () => <NBA_SVG className={styles.sports_logo} />
-  },
-  {
-    name: 'NFL',
-    id: 'NFL',
-    background: '#91442A',
-    logo: () => <NFL_SVG className={styles.sports_logo} />
-  },
-  {
-    name: 'MLB',
-    id: 'MLB',
-    background: '#1878FB',
-    logo: () => <MLB_SVG className={styles.sports_logo} />
-  },
-  {
-    name: 'NCAAF',
-    id: 'NCAAF',
-    background: '#91442A',
-    logo: () => <NFL_SVG className={styles.sports_logo} />
-  },
-  {
-    name: 'NCAAB',
-    id: 'NCAAB',
-    background: '#EC4C15',
-    logo: () => <NBA_SVG className={styles.sports_logo} />
-  },
-  {
-    name: 'SOCCER',
-    id: 'SOCCER',
-    background: '#6DCF40',
-    logo: () => <SOCCER_SVG className={styles.sports_logo} />
-  },
-  {
-    name: 'UFC',
-    id: 'UFC',
-    background: '#F9282B',
-    logo: () => <UFC_SVG className={styles.sports_logo} />
-  },
-  {
-    name: 'FORMULA 1',
-    id: 'F1',
-    background: '#505054',
-    logo: () => <F1_SVG className={styles.sports_logo} />
-  }
-];
+// const SPORTS_INFO = [
+//   {
+//     name: 'NBA',
+//     id: 'NBA',
+//     background: '#EC4C15',
+//     logo: () => <NBA_SVG className={styles.sports_logo} />
+//   },
+//   {
+//     name: 'NFL',
+//     id: 'NFL',
+//     background: '#91442A',
+//     logo: () => <NFL_SVG className={styles.sports_logo} />
+//   },
+//   {
+//     name: 'MLB',
+//     id: 'MLB',
+//     background: '#1878FB',
+//     logo: () => <MLB_SVG className={styles.sports_logo} />
+//   },
+//   {
+//     name: 'NCAAF',
+//     id: 'NCAAF',
+//     background: '#91442A',
+//     logo: () => <NFL_SVG className={styles.sports_logo} />
+//   },
+//   {
+//     name: 'NCAAB',
+//     id: 'NCAAB',
+//     background: '#EC4C15',
+//     logo: () => <NBA_SVG className={styles.sports_logo} />
+//   },
+//   {
+//     name: 'SOCCER',
+//     id: 'SOCCER',
+//     background: '#6DCF40',
+//     logo: () => <SOCCER_SVG className={styles.sports_logo} />
+//   },
+//   {
+//     name: 'UFC',
+//     id: 'UFC',
+//     background: '#F9282B',
+//     logo: () => <UFC_SVG className={styles.sports_logo} />
+//   },
+//   {
+//     name: 'FORMULA 1',
+//     id: 'F1',
+//     background: '#505054',
+//     logo: () => <F1_SVG className={styles.sports_logo} />
+//   }
+// ];
 
 export default function BannerSportsAndMatches() {
-  const [nextArrowVisible, setNextArrowVisible] = useState<boolean>(true);
-  const [prevArrowVisible, setPrevArrowVisible] = useState<boolean>(false);
-  const [sportMenuOpen, setSportMenuOpen] = useState<boolean>(false);
-  const [selectedSport, setSelectedSport] = useState<SportInfoType>(SPORTS_INFO[0]);
-  const carouselRef: RefObject<CarouselRef> = createRef();
+  // const [nextArrowVisible, setNextArrowVisible] = useState<boolean>(true);
+  // const [prevArrowVisible, setPrevArrowVisible] = useState<boolean>(false);
+  // const [sportMenuOpen, setSportMenuOpen] = useState<boolean>(false);
+  // const [selectedSport, setSelectedSport] = useState<SportInfoType>(SPORTS_INFO[0]);
+  //const carouselRef: RefObject<CarouselRef> = createRef();
 
-  const goCarouselBack = () => {
-    carouselRef.current?.prev();
-  };
-  const goCarouselNext = () => {
-    carouselRef.current?.next();
-  };
-  const changeSport = (sport: SportInfoType) => {
-    setSelectedSport(sport);
-    setSportMenuOpen(false);
-  };
-  const changeMenuVisible = (status: boolean) => {
-    setSportMenuOpen(status);
-  };
-  const beforeSlideChange = (_: number, next: number) => {
-    const slides = document.querySelectorAll('.matches-carousel .slick-dots li').length;
-    if (slides == next + 1) {
-      setNextArrowVisible(false);
-    } else if (next == 0) {
-      setPrevArrowVisible(false);
-    } else {
-      setPrevArrowVisible(true);
-      setNextArrowVisible(true);
-    }
-  };
+  // const goCarouselBack = () => {
+  //   carouselRef.current?.prev();
+  // };
+  // const goCarouselNext = () => {
+  //   carouselRef.current?.next();
+  // };
+  // const changeSport = (sport: SportInfoType) => {
+  //   setSelectedSport(sport);
+  //   setSportMenuOpen(false);
+  // };
+  // const changeMenuVisible = (status: boolean) => {
+  //   setSportMenuOpen(status);
+  // };
+  // const beforeSlideChange = (_: number, next: number) => {
+  //   const slides = document.querySelectorAll('.matches-carousel .slick-dots li').length;
+  //   if (slides == next + 1) {
+  //     setNextArrowVisible(false);
+  //   } else if (next == 0) {
+  //     setPrevArrowVisible(false);
+  //   } else {
+  //     setPrevArrowVisible(true);
+  //     setNextArrowVisible(true);
+  //   }
+  // };
 
-  const menu = (
-    <Menu className={styles.sportMenu}>
-      {SPORTS_INFO.map((sport) => (
-        <Menu.Item
-          key={sport.name}
-          className={styles.sportMenuItem}
-          onClick={() => changeSport(sport)}>
-          {sport.name}
-        </Menu.Item>
-      ))}
-    </Menu>
-  );
+  // const menu = (
+  //   <Menu className={styles.sportMenu}>
+  //     {SPORTS_INFO.map((sport) => (
+  //       <Menu.Item
+  //         key={sport.name}
+  //         className={styles.sportMenuItem}
+  //         onClick={() => changeSport(sport)}>
+  //         {sport.name}
+  //       </Menu.Item>
+  //     ))}
+  //   </Menu>
+  // );
 
-  const responsive = [
-    {
-      breakpoint: 1240,
-      settings: {
-        slidesToShow: 5,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 1120,
-      settings: {
-        slidesToShow: 4,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 940,
-      settings: {
-        slidesToShow: 3,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        dots: true
-      }
-    }
-  ];
+  // const responsive = [
+  //   {
+  //     breakpoint: 1240,
+  //     settings: {
+  //       slidesToShow: 5,
+  //       dots: true
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 1120,
+  //     settings: {
+  //       slidesToShow: 4,
+  //       dots: true
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 940,
+  //     settings: {
+  //       slidesToShow: 3,
+  //       dots: true
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       slidesToShow: 2,
+  //       dots: true
+  //     }
+  //   }
+  // ];
   return (
     <div className={styles.sports_and_matches}>
       <div className={styles.container}>
